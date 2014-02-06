540b6cebaf58ba8361dd334fbb937a67101d9066
RLieske <Lieske.H-D@partner.akdb.de>
2014-02-06 08:38:32 -0800

    Chg: refactored OrafileRenderer

--- src/main/java/edu/gatech/gtri/orafile/OrafileRenderer.java
+++ src/main/java/edu/gatech/gtri/orafile/OrafileRenderer.java
@@ -8,6 +8,13 @@ import java.util.regex.Pattern;
 
 public class OrafileRenderer {
 
+    protected static final String LINE_SEPARATOR = "\n";
+    // System.getProperty("line.separator");
+
+    private static final String APOSTROPHE = "\"";
+    private static final String BRACKET_OPEN = "(";
+    private static final String BRACKET_CLOSED = ")";
+
     final boolean sortByKey;
 
     public OrafileRenderer() {
@@ -19,8 +26,9 @@ public class OrafileRenderer {
     }
 
     /**
-     * @param sortByKey True to sort entries by key. False to preserve
-     *  original ordering.
+     * @param sortByKey
+     *            True to sort entries by key. False to preserve original
+     *            ordering.
      * @return A new {@link OrafileRenderer}.
      */
     public OrafileRenderer sortByKey(boolean sortByKey) {
@@ -46,102 +54,98 @@ public class OrafileRenderer {
         return writer.toString();
     }
 
-    public void renderFile(OrafileDict dict, Writer writer)
-            throws IOException {
+    public void renderFile(OrafileDict dict, Writer writer) throws IOException {
         Iterator<OrafileDef> defs = defs(dict).iterator();
         while (defs.hasNext()) {
-
             OrafileDef def = defs.next();
 
             renderDef(writer, def, Parens.No, "");
 
             if (defs.hasNext()) {
-                writer.append("\n");
+                writer.append(LINE_SEPARATOR);
             }
         }
     }
 
-    void renderDef(Writer writer, OrafileDef def, Parens parens,
-            String indent) throws IOException {
+    void renderDef(Writer writer, final OrafileDef def, final Parens parens, final String indent) throws IOException {
+        writer.append(indent);
+        if (parens.yes()) {
+            writer.append(BRACKET_OPEN);
+        }
 
-        String nextIndent = indent + "  ";
+        renderVal(writer, def.getName(), def.getVal(), parens, indent);
 
-        OrafileVal val = def.getVal();
+        if (parens.yes()) {
+            writer.append(BRACKET_CLOSED).append(LINE_SEPARATOR);
+        }
+    }
+
+    void renderVal(Writer writer, final String defName, final OrafileVal val, final Parens parens, final String indent)
+            throws IOException {
+        final String nextIndent = indent + "  ";
 
         if (val instanceof OrafileString) {
+            String stringVal = ((OrafileString) val).toString();
 
-            String stringVal = ((OrafileString) val).string;
-
-            writer.append(indent);
-            if (parens.yes()) writer.append("(");
-            writer.append(def.getName()).append(" = ");
+            writer.append(defName).append(" = ");
             renderString(writer, stringVal);
-            if (parens.yes()) writer.append(")\n");
-
         } else if (val instanceof OrafileStringList) {
-
             OrafileStringList stringListVal = (OrafileStringList) val;
 
-            writer.append(indent);
-            if (parens.yes()) writer.append("(");
-            writer.append(def.getName()).append(" = (\n");
-            Iterator<String> stringVals = stringListVal.list.iterator();
+            writer.append(defName).append(" = ").append(BRACKET_OPEN).append(LINE_SEPARATOR);
+            Iterator<String> stringVals = stringListVal.asStringList().iterator();
             while (stringVals.hasNext()) {
-
                 String stringVal = stringVals.next();
 
                 writer.append(nextIndent);
                 renderString(writer, stringVal);
-                if (stringVals.hasNext()) writer.append(",");
-                writer.append("\n");
+                if (stringVals.hasNext()) {
+                    writer.append(",");
+                }
+                writer.append(LINE_SEPARATOR);
             }
-            writer.append(indent).append(")");
-            if (parens.yes()) writer.append(")\n");
-
+            writer.append(indent).append(BRACKET_CLOSED);
         } else {
-
+            // val is instance of OrafileDict
             OrafileDict dict = (OrafileDict) val;
 
-            writer.append(indent);
-            if (parens.yes()) writer.append("(");
-            writer.append(def.getName()).append(" =\n");
+            writer.append(defName).append(" =").append(LINE_SEPARATOR);
+
             for (OrafileDef nextDef : defs(dict)) {
                 renderDef(writer, nextDef, Parens.Yes, nextIndent);
             }
 
-            if (parens.yes()) writer.append(indent).append(")\n");
+            if (parens.yes()) {
+                writer.append(indent);
+            }
         }
     }
 
-    static final Pattern SAFE_STRING =
-        Pattern.compile("^[A-Za-z0-9\\Q<>/.:;-_$+*&!%?@\\E]+$");
+    static final Pattern SAFE_STRING = Pattern.compile("^[A-Za-z0-9\\Q<>/.:;-_$+*&!%?@\\E]+$");
 
     void renderString(Writer writer, String string) throws IOException {
-
         if (SAFE_STRING.matcher(string).matches()) {
             writer.append(string);
         } else {
-            String escaped = string
-                .replaceAll("\\\\", "\\\\\\\\")
-                .replaceAll("\"", "\\\\\"");
-            writer.append("\"").append(escaped).append("\"");
+            String escaped = string.replaceAll("\\\\", "\\\\\\\\").replaceAll(APOSTROPHE, "\\\\\"");
+            writer.append(APOSTROPHE).append(escaped).append(APOSTROPHE);
         }
     }
 
     List<OrafileDef> defs(OrafileDict dict) {
-
-        if (!sortByKey) return dict.list;
-
-        List<OrafileDef> defs = new ArrayList<OrafileDef>(dict.list);
+        if (!this.sortByKey) {
+            return dict.asList();
+        }
+        List<OrafileDef> defs = new ArrayList<OrafileDef>(dict.asList());
         Collections.sort(defs, DEF_KEY_COMPARATOR);
         return defs;
     }
 
-    static final Comparator<OrafileDef> DEF_KEY_COMPARATOR =
-            new Comparator<OrafileDef>() {
+    static final Comparator<OrafileDef> DEF_KEY_COMPARATOR = new Comparator<OrafileDef>() {
         @Override
         public int compare(OrafileDef a, OrafileDef b) {
             return a.getName().compareTo(b.getName());
         }
     };
+
 }
